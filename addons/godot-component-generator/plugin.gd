0393afa5da1185fe1694b0307a24aa1796e8b863
bkadezabek <borna.kadezabek3@gmail.com>
2024-09-04 13:42:54 +0200

    Added plugin into the editor interface

--- addons/godot-component-generator/plugin.gd
+++ addons/godot-component-generator/plugin.gd
@@ -1,9 +1,31 @@
 @tool
 extends EditorPlugin
 
-func _enter_tree():
-	pass
+const MainPanel = preload("res://addons/godot-component-generator/main_panel.tscn")
 
+var main_panel_instance
+
+func _enter_tree():
+	main_panel_instance = MainPanel.instantiate()
+	# Add the main panel to the editor's main viewport.
+	EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
+	# Hide the main panel. Very much required.
+	_make_visible(false)
 
 func _exit_tree():
-	pass
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+func _has_main_screen():
+	return true
+
+func _make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+func _get_plugin_name():
+	return "Component Creator"
+
+func _get_plugin_icon():
+	# Must return some kind of Texture for the icon.
+	return EditorInterface.get_editor_theme().get_icon("ResourcePreloader", "EditorIcons")
